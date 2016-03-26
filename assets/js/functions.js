21c79b57395c334b537ceb003c60eff20c1194d9
Anatolii Olshevskyi <Anatolkruv@gogle.com>
2016-03-26 20:57:13 +0200

    create footer

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,9 +3,37 @@
 
   $(document).ready(function () {
 
+  	// animate button
+  	$('.ninja-btn').click( function() {
+      $(this).toggleClass('active');
+    });
 
+  	// hide and show site-navigation by click
+  	function menuSlide () {
+  		var $toggleNav = $('.toggle-nav');
+  		var $toggleBtn = $('.toggle-btn'); 
 
+  		if ($(window).width() <= 992) {
+  			$toggleNav.hide();
+  		}
 
+  		$toggleBtn.on('click', function(){
+  			$(this).next($toggleNav).slideToggle();
+  		});
+
+  		$(window).resize(function() {
+  			if ($(window).width() <= 992) {
+  				$toggleNav.hide();
+  				$('.ninja-btn').removeClass('active');
+
+  			} else if ($(window).width() > 992) {
+  				$toggleNav.show();
+  				$('.ninja-btn').removeClass('active');
+  			}
+  		});
+  	}
+
+    menuSlide ();
 
   }); //end ready
 
