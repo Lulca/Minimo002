5d4b5bde0a6018fafc29443e67b7b86668105842
Anatolii Olshevskyi <Anatolkruv@gogle.com>
2016-03-30 15:57:33 +0300

    set a transition property for slides

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,9 +1,12 @@
 (function ($) {
-	'use strict';
 
+
+	'use strict';
+    // begin ready
 	$(document).ready(function () {
 
-	(function (){
+    // drive nav
+   (function (){
   	// animate button
   	$('.ninja-btn').click( function() {
   		$(this).toggleClass('active');
@@ -11,6 +14,7 @@
 
   	// hide and show site-navigation by click
   	(function () {
+
   		var $toggleNav = $('.toggle-nav');
   		var $toggleBtn = $('.toggle-btn'); 
 
@@ -32,11 +36,33 @@
   				$('.ninja-btn').removeClass('active');
   			}
   		});
-  	}());
 
-  }());
+  	}()); // hide and show site-navigation by click
+
+  }()); // drive nav
+
+   // fefresh widths of .slider adn li in case img > 3 (defoul number)
+   (function () {
+
+    var $slider = $('.slider'),
+        $slides = $slider.find('.slides'),
+        $li = $slides.find('li'),
+        liNumber = $li.length;
+
+        if (liNumber != 3) {
+          $slides.css({'width': liNumber*100 + '%'});
+          $li.css({'width': 100/liNumber + '%'});
+        }
+
+        // drive slider work
+        (function () {
+
+        }()); // drive slider work
+
+   }()); // fefresh number of images of slider
 
 
   }); //end ready
+
 
 }(jQuery));
