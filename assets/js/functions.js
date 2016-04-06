016c2bf348a28df6e3db05c84fc6cc6dc3ac4224
Anatolii Olshevskyi <Anatolkruv@gogle.com>
2016-04-06 20:38:17 +0300

    first item do not toogle class active

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,12 +1,83 @@
-(function ($) {
+var slider = (function() {
+
+  return {
+
+    init: function() {
+
+      var that = this;
+
+      $('.control-button').on('click', function() {
+        console.log('heloo');
+        var $this = $(this),
+        slides = $this.closest('.slider').find('.slider-item'),
+        active = slides.filter('.active'),
+        prevSlide = active.prev(),
+        nextSlide = active.next(),
+        first = slides.first(),
+        last = slides.last();
+
+        if ( $('.rightArrow') ) {
+
+          that.moveSlide(nextSlide, 'forward');
+
+        } else if ( $('.leftArrow') ) {
+
+          that.moveSlide(prevSlide, 'backward');
+
+        }
 
 
-	'use strict';
-    // begin ready
-	$(document).ready(function () {
+      });
 
-   
-  }); //end ready
+    },
 
+    moveSlide: function(slide, direction) {
 
-}(jQuery));
+      var
+        slider = slide.closest('.slider'),
+        slides = slider.find('.slider-item'),
+        activeSlide = slides.filter('.active'),
+        slideWidth = slides.width(),
+        duration = 500,
+        reqCssPositon = 0,
+        reqSlideStrafe = 0;
+
+        if (direction === 'forward') {
+
+          reqCssPositon = slideWidth;
+          reqSlideStrafe = -slideWidth;
+
+        } else if (direction === 'backward') {
+
+          reqCssPositon = -slideWidth;
+          reqSlideStrafe = slideWidth;
+
+        }
+
+        slide.css({'left': reqCssPositon +'px'}).addClass('inslide');
+
+        var movebleSlide = slides.filter('.inslide');
+
+        activeSlide.animate({left: reqSlideStrafe}, duration);
+        movebleSlide.animate({left: 0}, duration, function() {
+
+          var $this = $(this);
+
+          slide.css({'left': '0'}).removeClass('active');
+          $this.toggleClass('inslide active');
+
+        });
+
+    }
+
+  }
+
+}());
+
+$(document).ready(function() {
+
+  if ( $('.slider').length ) {
+    slider.init();
+  }
+
+});
