4d62948e298c73eb8f36770550e69ab19a54f9fc
Anatolii Olshevskyi <Anatolkruv@gogle.com>
2016-04-06 12:56:54 +0300

    initial commit in branch correctSlider

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -5,46 +5,7 @@
     // begin ready
 	$(document).ready(function () {
 
-    // drive nav
-   (function (){
-  	// animate button
-  	$('.ninja-btn').click( function() {
-  		$(this).toggleClass('active');
-  	});
-
-  	// hide and show site-navigation by click
-  	(function () {
-
-  		var $toggleNav = $('.toggle-nav');
-  		var $toggleBtn = $('.toggle-btn'); 
-
-  		if ($(window).width() <= 992) {
-  			$toggleNav.hide();
-  		}
-
-  		$toggleBtn.on('click', function(){
-  			$(this).next($toggleNav).slideToggle();
-  		});
-
-  		$(window).resize(function() {
-  			if ($(window).width() <= 992) {
-  				$toggleNav.hide();
-  				$('.ninja-btn').removeClass('active');
-
-  			} else if ($(window).width() > 992) {
-  				$toggleNav.show();
-  				$('.ninja-btn').removeClass('active');
-  			}
-
-
-  		});
-
-  	}()); // hide and show site-navigation by click
-
-  }()); // drive nav
-
- 
-     
+   
   }); //end ready
 
 
